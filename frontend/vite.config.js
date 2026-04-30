dbbf8439e79320ab4f00219c5177d44dc8472744
leonhnguyen <lleonhn@gmail.com>
2026-04-29 18:31:25 -0700

    ficed vite.config.js file so website can be deployed.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+import react from '@vitejs/plugin-react'
+import babel from '@rollup/plugin-babel'
 
 export default defineConfig({
   base: "/",
-});
+  plugins: [
+    react(),
+    babel({
+      presets: ['@babel/preset-react']
+    })
+  ]
+})
